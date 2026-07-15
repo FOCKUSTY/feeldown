44fc25ef1ca1d9a06eed63e06ebb72ecb0d6e4e6
FOCKUSTY <wwddwwwd@mail.ru>
2026-07-15 18:45:36 +0500

    feat: added adaptive

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export default async (req, res) => {
-  const { reqHandler } = await import('../dist/feeldown/server/server.mjs');
+  const { reqHandler } = await import("../dist/feeldown/server/server.mjs");
   return reqHandler(req, res);
 };
