ab3f0f5611786f572443871dc357cb2cda84b2a5
FOCKUSTY <wwddwwwd@mail.ru>
2026-07-15 18:01:01 +0500

    fix: updated prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'posts/new',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'posts/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'users/:slug',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server,
   },
 ];
